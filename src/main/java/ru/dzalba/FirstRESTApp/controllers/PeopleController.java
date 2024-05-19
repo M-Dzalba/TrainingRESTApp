18ce33996e293c9fa31dbcc483489a30fbee05b0
Maxim Dzalba <brest-dzalba@list.ru>
2024-05-19 19:51:38 +0300

    added: new errorhandler, post request

--- src/main/java/ru/dzalba/FirstRESTApp/controllers/PeopleController.java
+++ src/main/java/ru/dzalba/FirstRESTApp/controllers/PeopleController.java
@@ -1,12 +1,16 @@
 package ru.dzalba.FirstRESTApp.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import ru.dzalba.FirstRESTApp.models.Person;
 import ru.dzalba.FirstRESTApp.services.PeopleService;
 import ru.dzalba.FirstRESTApp.util.PersonErrorResponse;
+import ru.dzalba.FirstRESTApp.util.PersonNotCreatedException;
 import ru.dzalba.FirstRESTApp.util.PersonNotFoundException;
 
 import java.util.List;
@@ -27,6 +31,24 @@ public class PeopleController {
     public Person getPerson(@PathVariable("id")int id){
        return peopleService.findOne(id);
     }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person,
+                                             BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            StringBuilder errorMsg=new StringBuilder();
+            List<FieldError>errors=bindingResult.getFieldErrors();
+            for(FieldError error:errors){
+                errorMsg.append(error.getField())
+                        .append(" - ").append(error.getDefaultMessage())
+                        .append("; ");
+            }
+            throw new PersonNotCreatedException(errorMsg.toString());
+        }
+        peopleService.save(person);
+        return ResponseEntity.ok(HttpStatus.OK); //пустое тело со статусом 200
+    }
+
     @ExceptionHandler
     private ResponseEntity<PersonErrorResponse>handleException(PersonNotFoundException e){
         PersonErrorResponse response=new PersonErrorResponse(
@@ -34,5 +56,13 @@ public class PeopleController {
                 System.currentTimeMillis()
         );
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    private ResponseEntity<PersonErrorResponse>handleException(PersonNotCreatedException e){
+        PersonErrorResponse response=new PersonErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);//500
     }
 }
